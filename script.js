0da60f91f4451a9f6eff129d8f7e27edde88baa0
loiramartins <135634328+loiramartins@users.noreply.github.com>
2023-09-25 12:37:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("LOIRA");
+function pom(){
+    documentos.querSelector('#som_tecla_pom').play();
+    }
+    documentos.querSelector('.tecla_clap').onclick = clap;
+
+function pom(){
+    documentos.querSelector('#som_tecla_clap').play();
+}
+documentos.querSelector('.tecla_clap').onclick = clap;
+
