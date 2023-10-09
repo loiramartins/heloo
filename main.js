e7e00ef24e998b1dabb726e7ec603f3a7f0db260
loiramartins <135634328+loiramartins@users.noreply.github.com>
2023-10-09 12:27:59 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function pom(){
+document.querySelector('#som_tecla_pom').play();
+}
+function clap(){
+document.querySelector('#som_tecla_clap').play();
+}
+function tim(){
+document.querySelector('#som_tecla_tim').play();
+}
+function puff(){
+document.querySelector('#som_tecla_puff').play();
+}
+function tim(){
+document.querySelector('#som_tecla_tim').play();
+}
+
+listaDeTeclas[0].onclick = pom;
